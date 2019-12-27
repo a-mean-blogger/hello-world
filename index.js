530c5879da989f8ec797dac53506721727be9133
a.mean.blogger <a.mean.blogger@gmail.com>
2019-12-27 08:57:52 -0800

    static folder added

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
+app.use(express.static(__dirname + '/public'));
 
 var port = 3000;
 app.listen(port, function(){
