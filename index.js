903f401205a859967291a63624c9100a6408718e
a.mean.blogger <a.mean.blogger@gmail.com>
2019-12-27 08:59:17 -0800

    hello route added

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine','ejs');
 app.use(express.static(__dirname + '/public'));
+
+app.get('/hello', function(req,res){
+  res.render('hello', {name:req.query.nameQuery});
+});
+
+app.get('/hello/:nameParam', function(req,res){
+  res.render('hello', {name:req.params.nameParam});
+});
 
 var port = 3000;
 app.listen(port, function(){
